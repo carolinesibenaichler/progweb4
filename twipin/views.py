39e0676662c30858c3db3985c86689b022ba4c6a
KennyaLacerda <kennyalacerda7@gmail.com>
2018-10-10 20:44:29 -0300

    funcionando adicionar e o listar

--- twipin/views.py
+++ twipin/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from .models import Twip
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
    contexto = {} 
    return render(request, 'twipin/index.html', contexto)
 
-
+@csrf_exempt
 def api_twips(request):
   if request.method == "GET" :
     twips = Twip.objects.all()
@@ -17,6 +18,10 @@ def api_twips(request):
       jt ["lista"].append(temp)
     return JsonResponse(jt)
   elif request.method == "POST":
-    #TODO salvar no baco
-    jt = { "resposta": "ok"}
+    t = request.POST["texto"]
+    #salva no banco
+    usuario = User.objects.get(username='admin')
+    twip = Twip(autor = usuario, texto = t)
+    twip.save()  
+    jt = { "mensagem": "ok"}
     return JsonResponse(jt)
