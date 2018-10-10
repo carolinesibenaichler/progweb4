37e99ed894534c525787dd56ee5e646ee6aa08f3
KennyaLacerda <kennyalacerda7@gmail.com>
2018-10-10 19:59:45 -0300

    mudando a urls para metos rest

--- twipin/views.py
+++ twipin/views.py
@@ -8,30 +8,12 @@ def index(request):
    return render(request, 'twipin/index.html', contexto)
 
 
-def json_exemplo(request):
-   
-   
-
-   pessoa = {
-       "nome": "João",
-       "idade": 18,
-       "parentes": {
-           "mãe": "Maria",
-           "pai": "Pedro",
-           "irmãos": 2
-       }
-   }
-
-   return JsonResponse(pessoa)
-
-
-def json_listar_twips(request):
-  twips = Twip.objects.all()
-
-  jt =  {"lista":[]} 
-  for t in twips:
-     temp = {"id": t.id, "texto": t.texto, "autor":t.autor.username}
-     jt ["lista"].append(temp)
-
-  return JsonResponse(jt)
+def api_twips(request):
+  if request.method == "GET" :
+    twips = Twip.objects.all()
+    jt =  {"lista":[]} 
+    for t in twips:
+      temp = {"id": t.id, "texto": t.texto, "autor":t.autor.username}
+      jt ["lista"].append(temp)
+    return JsonResponse(jt)
     
